5482c9cf520538f847bd76be08d3aadeff816635
DimaNikishin <dimanikishin@yandex.ru>
2017-03-06 21:38:06 +0200

    feature:login logic

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -2,21 +2,54 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as UserActions from '../../actions/UserActions';
+import * as ApiUrl from '../../constants/Routes';
 
 export class LoginPage extends Component {
     //TODO
-    // constructor() {
-    //     super();
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {userEmail: '', userPassword: ''};
+
+        this.requestHeader = new Headers({
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        })
+
+        this.fetchOpt = {
+            method: 'POST', 
+            headers: this.requestHeader,
+            mode: 'cors'
+        }
+
+        this.loginUrl = ApiUrl.ROOT_API + ApiUrl.LOGIN_API;
+    }
+
     componentDidMount() {
         document.body.classList.remove('gray-bg');
     }
-    handleSubmit(e) {
-        e.preventDefault();
-        this.props.actions.login({
-            email: e.target.elements[0].value,
-            password: e.target.elements[1].value
-        });
+
+    handleChangeEmail(event) {
+        this.setState({userEmail: event.target.value});
+    }
+
+    handleChangePassword(event) {
+        this.setState({userPassword: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.fetchOpt.body = JSON.stringify({email: this.state.userEmail, password: this.state.userPassword});
+        //TODO add seccuse and fail logic
+        fetch(this.loginUrl, this.fetchOpt)
+            .then(res => { console.log(res); })
+            .catch(error => { console.log(error) });
+
+        // e.preventDefault();
+        // this.props.actions.login({
+        //     email: e.target.elements[0].value,
+        //     password: e.target.elements[1].value
+        // });
 
         /*const login = e.target.elements[0].value;
          window.localStorage.setItem('rr_login', login);
@@ -42,13 +75,17 @@ export class LoginPage extends Component {
                                     <input type="text"
                                            name="userEmail"
                                            className="form-control"
-                                           placeholder="Email" />
+                                           placeholder="Email"
+                                           value={this.state.userEmail} 
+                                           onChange={::this.handleChangeEmail} />
                                 </div>
                                 <div className="form-group">
                                     <input type="password"
                                            name="userPassword"
                                            className="form-control"
-                                           placeholder="Password" />
+                                           placeholder="Password" 
+                                           value={this.state.userPassword} 
+                                           onChange={::this.handleChangePassword}/>
                                 </div>
                                 <button type="submit"
                                         className="btn btn-primary block full-width m-b"

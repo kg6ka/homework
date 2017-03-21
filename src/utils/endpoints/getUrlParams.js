9d4089dd87fb3145dd6f1f27f8aeefa447388031
DimaNikishin <dimanikishin@yandex.ru>
2017-03-21 21:44:29 +0200

    get params fix

--- src/utils/endpoints/getUrlParams.js
+++ src/utils/endpoints/getUrlParams.js
@@ -1,4 +1,4 @@
 export const getParams = (name, url = window.location.href) => {
 	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(url);
-	return results[1] || 0;
+	return results ? results[1] : false;
 }
